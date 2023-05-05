fdcafdd3aa6e5339f5980f32962a3c0471a8022b
Brian Sanchez Aguilar <briansanchezaguilar@gmail.com>
2023-05-05 10:46:47 -0700

    Se agrega base del proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { jsPDF } from "jspdf";
+import * as csvToJson from 'csvtojson';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,62 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'csvpayment';
+  fileCharged: File;
+  private jsonData: any;
+
+
+  constructor() {
+    this.fileCharged = null
+    this.jsonData = null
+  }
+
+  public onFileSelected(event: any) {
+    this.fileCharged = event.target.files[0];
+
+    const reader = new FileReader();
+    reader.readAsText(this.fileCharged);
+
+    reader.onload = () => {
+
+      const csvData = reader.result as string;
+      csvToJson().fromString(csvData).then((jsonData) => {
+        this.jsonData = jsonData
+        console.log(jsonData)
+      });
+
+    };
+  }
+
+
+
+  public makePdf() {
+
+    const doc = new jsPDF();
+    // Json to String
+    const jsonData = JSON.stringify(this.jsonData, null, 4);
+
+    // const headers = [['Nombre', 'Apellido', 'Dirección', 'Ciudad', 'Estado', 'Código Postal']];
+    // const rows = [];
+
+    // this.jsonData.forEach((dato) => {
+    //   const nombre = dato['John'];
+    //   const apellido = dato['Doe'];
+    //   const direccion = dato['120 jefferson st.'];
+    //   const ciudad = dato['Riverside'];
+    //   const estado = dato['NJ'];
+    //   const codigoPostal = dato['08075'];
+
+    //   const row = [nombre, apellido, direccion, ciudad, estado, codigoPostal];
+    //   rows.push(row);
+    // });
+
+    doc.text(jsonData, 0, 20);
+
+
+    doc.text('Nomina', 10, 10);
+    doc.save('Nomina.pdf');
+
+  }
+
+
 }
